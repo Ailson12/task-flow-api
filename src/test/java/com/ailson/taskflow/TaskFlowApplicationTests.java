e255587fe39a812d4073c118125fa676a3e1d454
ailson <robertofeitosa06@gmail.com>
2024-10-13 14:43:48 -0400

    first commit

---
+++ src/test/java/com/ailson/taskflow/TaskFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ailson.taskflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TaskFlowApplication.class)
+class TaskFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
